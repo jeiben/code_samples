04c88dc590d03d66c1f090407a7708a19ec04898
jeiben <jeiben@kippnyc.org>
2015-05-04 15:34:10 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -2,12 +2,12 @@ from random import randint
 
 tracker = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
 
-for i in range(10000):
+for i in range(500):
     roll = randint(1,6) + randint(1,6)
     tracker[roll] = tracker[roll] + 1
 
 for combination in tracker:
-    print "%s rolled %s times out of 10000" %(combination, tracker[combination])
-    prob = float(tracker[combination])/10000
+    print "%s rolled %s times out of 500" %(combination, tracker[combination])
+    prob = float(tracker[combination])/500
     percent = prob*100
     print "This is a probability of %s, or %s percent!" %(prob, percent)
