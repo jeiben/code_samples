9fb6728ac5c495db9740f5381bca0251dd2a7ffb
jeiben <jeiben@kippnyc.org>
2016-01-28 16:59:21 -0500

    clean folder and creat syntax basics document

---
+++ python_syntax_basics.py
@@ -0,0 +1,13 @@
+## Python syntax basics
+## comments start with '##'
+
+myNum = 5  ##to make a variable, create a name and set it equal to something
+yourNum = 9
+isLarger = true
+
+ourNumSum = myNum + yourNum  ##variables can reference each other
+
+print myNump  ##you can print numbers
+print isLarger  ##you can print booleans
+print "this is a string"  ##you can print strings
+print "our sum is: ", ourNumSum  ##you can concatenate (add together) different types and print them
