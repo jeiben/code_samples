bedee6a3f96220333027d45ffd6d7c90d9e8a078
jeiben <jeiben@kippnyc.org>
2016-02-01 10:15:57 -0500

    Create Sample.java

---
+++ java/Sample.java
@@ -0,0 +1,18 @@
+/**A basic sample class*/
+
+public class Sample
+{
+  private int num;
+  private String name;
+  
+  public Sample(int myNum, String myName)
+  {
+    num = myNum;
+    name = myName;
+  }
+  
+  public String getName()
+  {
+    return name;
+  }
+}
