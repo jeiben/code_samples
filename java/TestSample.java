ab33ec803fb7d54e36822315350c00b6fe49bfef
jeiben <jeiben@kippnyc.org>
2016-02-01 10:17:26 -0500

    Create TestSample.java

---
+++ java/TestSample.java
@@ -0,0 +1,9 @@
+/**Tests the Sample Class*/
+
+public TestSample
+{
+  public static void main(String[] args) {
+        Sample s1 = new Sample(7, "Fred");
+        System.out.println(s1.getName());
+    }
+}
